0f351f3659bc545a201abe8f2f6a1f3024f363aa
AI-Hub-Admin <aihubadmin@126.com>
2024-10-16 12:41:36 +0800

    Create constants.py

---
+++ src/tencent/constants.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/06/27
+
+## START SCREEN ENABLE
+START_SCREEN_ENABLE = True
+
+## KEY
+KEY_API_NAME = "api_name"
+KEY_IMPL = "impl"
+
+API_NAME_BASE = "api_base"
+API_NAME_DEMO = "api_demo"
+API_NAME_STOCK = "stock_price"
+API_NAME_DIALOGUE_CHALLENGE = "dialogue_challenge"
+
